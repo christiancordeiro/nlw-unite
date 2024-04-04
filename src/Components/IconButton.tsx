ec04c21996c33d3764a49db55ae78a242e3ce985
Christian Cordeiro <christiancordeiro96@gmail.com>
2024-04-03 21:26:09 -0300

    correct import

--- src/Components/IconButton.tsx
+++ src/Components/IconButton.tsx
@@ -1,4 +1,4 @@
-import React, { ComponentProps } from "react"
+import { ComponentProps } from "react"
 import { twMerge } from "tailwind-merge"
 
 interface IconButtonProps extends ComponentProps<"button"> {
--- src/Components/Table/TableCell.tsx
+++ src/Components/Table/TableCell.tsx
@@ -1,4 +1,4 @@
-import React, { ComponentProps } from "react"
+import { ComponentProps } from "react"
 import { twMerge } from "tailwind-merge"
 
 interface TableCellProps extends ComponentProps<"td"> {}
